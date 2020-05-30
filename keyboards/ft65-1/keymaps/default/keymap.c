db5509f83e69c62b2e4f3cca640d32b5716adf95
Yago Santos <yagossc@gmail.com>
2020-05-30 00:34:55 -0300

    Add FN layer

--- keyboards/ft65-1/keymaps/default/keymap.c
+++ keyboards/ft65-1/keymaps/default/keymap.c
@@ -34,21 +34,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSLASH, KC_INSERT,
                      KC_Y, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_ENTER, KC_SCROLLLOCK,
                      KC_H, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_LSHIFT, KC_PGUP, KC_PSCREEN,
-                     KC_N, KC_SPACE, KC_RALT, KC_SLASH, KC_HOME, KC_PGDOWN, KC_END,
+                     KC_N, KC_SPACE, KC_RALT, 0xD3, KC_HOME, KC_PGDOWN, KC_END,
                      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,
                      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_6,
                      KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_T,
                      KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_G,
-                     KC_CAPS, KC_LCTRL, KC_LGUI, KC_LALT, KC_LCTRL, KC_B
-                     )
-    /* [_BASE] = LAYOUT( */
-    /*     KC_A,    KC_1,    MO(_FN), */
-    /*         KC_TAB,   KC_SPC */
-    /* ), */
-    /* [_FN] = LAYOUT( */
-    /*     QMKBEST, QMKURL,  _______, */
-    /*         RESET,    XXXXXXX */
-    /* ) */
+                     MO(_FN), KC_LCTRL, KC_LGUI, KC_LALT, KC_LCTRL, KC_B
+                     ),
+    [_FN] = LAYOUT(
+                   KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,
+                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO,
+                   KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT,
+                   KC_GRAVE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
+                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+                   )
 };
 
 /* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
