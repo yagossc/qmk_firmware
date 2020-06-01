8bcd3267435618919e2fb2dbd99a14be4bae1358
Yago Santos <yagossc@gmail.com>
2020-06-01 20:26:20 -0300

    Better adapt keymap to personal preferences

--- keyboards/ft65-1/keymaps/default/keymap.c
+++ keyboards/ft65-1/keymaps/default/keymap.c
@@ -27,15 +27,16 @@ enum layer_names {
 /*     QMKURL */
 /* }; */
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
                      KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSPACE, KC_DEL,
-                     KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSLASH, KC_INSERT,
-                     KC_Y, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_ENTER, KC_SCROLLLOCK,
-                     KC_H, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_LSHIFT, KC_PGUP, KC_PSCREEN,
-                     KC_N, KC_SPACE, KC_RALT, 0xD3, KC_HOME, KC_PGDOWN, KC_END,
-                     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,
+                     KC_U, KC_I, KC_O, KC_P, KC_QUOTE, KC_LBRACKET, KC_BSLASH, KC_INSERT,
+                     KC_Y, KC_J, KC_K, KC_L, KC_GRAVE, KC_RBRACKET, KC_ENTER, KC_SCROLLLOCK,
+                     KC_H, KC_M, KC_COMMA, KC_DOT, KC_SCOLON, KC_LSHIFT, KC_PGUP, KC_PSCREEN,
+                     KC_N, KC_SPACE, KC_RALT, KC_SLASH, KC_HOME, KC_PGDOWN, KC_END,
+                     KC_QUOTE, KC_1, KC_2, KC_3, KC_4, KC_5,
                      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_6,
                      KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_T,
                      KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_G,
@@ -47,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO,
                    KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT,
-                   KC_GRAVE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
+                   KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
