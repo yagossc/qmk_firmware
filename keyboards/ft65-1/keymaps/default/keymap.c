cb17eb2819d7605be4e2346a1719ca5dc9db9618
Yago Santos <yagossc@gmail.com>
2020-06-03 20:07:34 -0300

    Add small fix to better suit default layout

--- keyboards/ft65-1/keymaps/default/keymap.c
+++ keyboards/ft65-1/keymaps/default/keymap.c
@@ -31,14 +31,13 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-
     [_BASE] = LAYOUT(
                      KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSPACE, KC_DEL,
                      KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSLASH, KC_INSERT,
-                     KC_Y, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_ENTER, TG(_TALK),
+                     KC_Y, KC_J, KC_K, KC_L, KC_SCOLON, KC_GRAVE, KC_ENTER, TG(_TALK),
                      KC_H, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, KC_PGUP, KC_PSCREEN,
                      KC_N, KC_SPACE, KC_RALT, KC_SLASH, KC_HOME, KC_PGDOWN, KC_END,
-                     KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5,
+                     KC_QUOTE, KC_1, KC_2, KC_3, KC_4, KC_5,
                      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_6,
                      KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_T,
                      KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_G,
