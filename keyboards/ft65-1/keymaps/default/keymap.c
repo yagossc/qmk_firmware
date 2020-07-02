50138016873cd901cac7880b06c03fb626f0e75e
Yago Santos <yagossc@gmail.com>
2020-07-02 15:12:07 -0300

    Create movement layer (_MV)

--- keyboards/ft65-1/keymaps/default/keymap.c
+++ keyboards/ft65-1/keymaps/default/keymap.c
@@ -19,7 +19,7 @@
 enum layer_names {
     _BASE,
     _FN,
-    _TALK
+    _MV
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
                      KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSPACE, KC_DEL,
                      KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSLASH, KC_INSERT,
-                     KC_Y, KC_J, KC_K, KC_L, KC_SCOLON, KC_GRAVE, KC_ENTER, TG(_TALK),
+                     KC_Y, KC_J, KC_K, KC_L, KC_SCOLON, KC_GRAVE, KC_ENTER, TG(_MV),
                      KC_H, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, KC_PGUP, KC_PSCREEN,
                      KC_N, KC_SPACE, KC_RALT, KC_SLASH, KC_HOME, KC_PGDOWN, KC_END,
                      KC_QUOTE, KC_1, KC_2, KC_3, KC_4, KC_5,
@@ -42,17 +42,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_G,
                      MO(_FN), KC_LCTRL, KC_LGUI, KC_LALT, KC_LCTRL, KC_B
                      ),
-    [_TALK] = LAYOUT(
-                     KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSPACE, KC_DEL,
-                     KC_U, KC_I, KC_O, KC_P, KC_QUOTE, KC_LBRACKET, KC_BSLASH, KC_INSERT,
-                     KC_Y, KC_J, KC_K, KC_L, KC_GRAVE, KC_RBRACKET, KC_ENTER, TG(_TALK),
-                     KC_H, KC_M, KC_COMMA, KC_DOT, KC_SCOLON, KC_RSHIFT, KC_HOME, KC_PSCREEN,
-                     KC_N, KC_SPACE, KC_RALT, KC_SLASH, KC_PGUP, KC_END, KC_PGDOWN,
-                     KC_QUOTE, KC_1, KC_2, KC_3, KC_4, KC_5,
-                     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_6,
-                     KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_T,
-                     KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_G,
-                     MO(_FN), KC_LCTRL, KC_LGUI, KC_LALT, KC_LCTRL, KC_B
+    [_MV] = LAYOUT(
+                     KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_NO, KC_NO, KC_NO,
+                     KC_NO, KC_UP, KC_NO, KC_MS_WH_UP, KC_NO, KC_NO, KC_NO, KC_NO,
+                     KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_NO, KC_MS_BTN3, TG(_MV),
+                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                     KC_MS_WH_DOWN, KC_MS_BTN1, KC_MS_BTN2, KC_NO, KC_NO, KC_NO, KC_NO,
+                     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,
+                     KC_TAB, KC_NO, KC_MS_UP, KC_NO, KC_NO, KC_NO,
+                     KC_NO, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_NO, KC_NO,
+                     KC_LSHIFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                     MO(_FN), KC_NO, KC_LGUI, KC_NO, KC_LCTRL, KC_NO
                      ),
     [_FN] = LAYOUT(
                      KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,
