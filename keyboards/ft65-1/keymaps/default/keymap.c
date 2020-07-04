7ad3f3c37b5650af065ca46e275d06882c12ddc1
Yago Santos <yagossc@gmail.com>
2020-07-04 13:37:28 -0300

    Add mouse button-2 to FN layer

--- keyboards/ft65-1/keymaps/default/keymap.c
+++ keyboards/ft65-1/keymaps/default/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_NO, KC_UP, KC_NO, KC_MS_WH_UP, KC_NO, KC_NO, KC_NO, KC_NO,
                      KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_NO, KC_MS_BTN3, KC_NO,
                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO,
-                     KC_MS_WH_DOWN, KC_MS_BTN1, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT,
+                     KC_MS_WH_DOWN, KC_MS_BTN1, KC_MS_BTN2, KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT,
                      KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
                      KC_NO, KC_NO, KC_MS_UP, KC_NO, KC_NO, KC_F6,
                      KC_NO, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_NO, KC_NO,
